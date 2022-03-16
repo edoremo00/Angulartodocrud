da80be7142282006b9519685346a75a716d67334
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2022-03-16 22:23:55 +0100

    file di test creati da angular

---
+++ src/app/services/changetodovisualization.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ChangetodovisualizationPipe } from './changetodovisualization.pipe';
+
+describe('ChangetodovisualizationPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ChangetodovisualizationPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
