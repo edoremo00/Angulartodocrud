e222a8c0f1bec3fd34300a618add30f5505e9fa1
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2022-03-16 22:21:26 +0100

    aggiunta pagina todo a dichiarazioni route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { RegisterComponent } from './components/register/register.component';
+import { TodoComponent } from './components/todo/todo.component';
 
 const routes: Routes = [
   {path:'home',component:HomepageComponent},
   {path:"",redirectTo:"/home",pathMatch:"full"},//setta pagina di default
-  {path:'register',component:RegisterComponent}
+  {path:'register',component:RegisterComponent},
+  {path:'todo',component:TodoComponent}
 ];
 
 @NgModule({
