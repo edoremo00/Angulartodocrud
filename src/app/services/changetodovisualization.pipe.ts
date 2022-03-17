a5791e4fbe1093cfa030275de4a29a62e57988c5
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2022-03-17 18:54:30 +0100

    resa pipe inpura per permettere corretto funzionamento in seguito ad update del todo valore fatto/non fatto

--- src/app/services/changetodovisualization.pipe.ts
+++ src/app/services/changetodovisualization.pipe.ts
@@ -2,17 +2,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Itodointerface } from 'src/interfaces/todointeerface';
 
 @Pipe({
-  name: 'changetodovisualization'
+  name: 'changetodovisualization',
+  pure:false //ho dovuto rendere pipe inpura per farla eseguire ad ogni cambio di stato sennò valore checkbox todo non era coerente
 })
 export class ChangetodovisualizationPipe implements PipeTransform { //pipe per mostrare-nascondere to do fatti o meno
 
   transform(value: Array<Itodointerface>, args?: string): Itodointerface[] {
-    //debugger;
-    
     if(!args){
       return value
-    }else if(args==="change todo visualization"){
-      return value;
     }else if(args==="1"){
       return value.filter(x=>x.isTodoDone)
     }else{
