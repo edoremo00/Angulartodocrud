9f25816509051e154a90ba05fcde2fc1008c747a
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2022-04-02 19:14:34 +0200

    aggiunta dichiarazione componente edit profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { RegisterComponent } from './components/register/register.component';
 import { TodoComponent } from './components/todo/todo.component';
+import { EditprofileComponent } from './editprofile/editprofile.component';
 
 const routes: Routes = [
   {path:'home',component:HomepageComponent},
   {path:"",redirectTo:"/home",pathMatch:"full"},//setta pagina di default
   {path:'register',component:RegisterComponent},
-  {path:'todo',component:TodoComponent}
+  {path:'todo',component:TodoComponent},
+  {path:'editprofile',component:EditprofileComponent}
 ];
 
 @NgModule({
