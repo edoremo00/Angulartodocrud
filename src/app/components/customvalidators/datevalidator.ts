703af49323a41ee539236a084bb7c46ee5c2a7b0
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2022-04-29 22:20:33 +0200

    aggiunti vari validator per le date

--- src/app/components/customvalidators/datevalidator.ts
+++ src/app/components/customvalidators/datevalidator.ts
@@ -5,11 +5,30 @@ export function validatedate():ValidatorFn{//:interfaccia riceve in input un con
     return (control: AbstractControl): ValidationErrors | null => {//in seconda graffa ho cosa deve restituire validator
         let dataoggi=new Date()
         //problema è data oggi
-        
         //let stringadataoggi=dataoggi.toLocaleDateString('').split('/').join('-');
 		if(control.value===moment(dataoggi).format('YYYY-MM-DD')){
             return {validatedate:true}
         }
 		return null;
 	};
+}
+
+export function validatefuturedates():ValidatorFn{
+    return(control:AbstractControl):ValidationErrors|null=>{
+        let dataoggi=new Date()
+        if(control.value>moment(dataoggi).format('YYYY-MM-DD')){
+            return {validatefuturedates:true}
+        }
+        return null;
+    }
+}
+export function atleast13yearsold():ValidatorFn{
+    return(control:AbstractControl):ValidationErrors|null=>{
+        let dataoggi=new Date()
+        let controlvalue:String=String(control.value).split('-')[0];
+        if(dataoggi.getFullYear()-Number(controlvalue)<13){
+            return {atleast13yearsold:true}
+        }
+        return null;
+    }
 }
