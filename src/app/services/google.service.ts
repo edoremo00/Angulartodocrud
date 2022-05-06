132fe72b198da55e20d90e98125b7a755ef7f7cb
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2022-05-06 18:24:11 +0200

    iniziato a testare un pò metodi per inserire eventi in calendario Google e ottenere i calendari dell'utente Google loggato

--- src/app/services/google.service.ts
+++ src/app/services/google.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
+import * as moment from 'moment';
 import { Observable, ReplaySubject } from 'rxjs';
 
 @Injectable({
@@ -8,15 +9,73 @@ import { Observable, ReplaySubject } from 'rxjs';
 export class GoogleService {
 
   private auth2:gapi.auth2.GoogleAuth|undefined
+  //private calendar:any //CAPIRE SE SI PUò FARE COME FATTO CON VARIABILE AUTH2 MA NON PENSO
+  //OGNI VOLTA DEVO QUINDI CARICARMI L'API?
   private subject=new ReplaySubject<gapi.auth2.GoogleUser|null>(1)//avrà valori null o utente a seconda se sei loggato o meno
   googleloggeduser:any
+  
+  
   constructor(private router:Router,private ngzone:NgZone) {
+    //this.LoadCalendarApi()
+    
     gapi.load('auth2',()=>{
+     
+     //let request= this.calendar=gapi.client.load('calendar','v3')
      this.auth2= gapi.auth2.init({
-        client_id:''
+        client_id:'',
+        scope:'https://www.googleapis.com/auth/calendar'//FONDAMENTALE METTERLO SENNO CALENDARIO NON ANDRà MAI
       })
     })
+    //QUESTO CODICE è SUPERFLUO. NON SERVE
+    /*gapi.load('calendar',()=>{
+      this.calendar=gapi.client.init({
+        clientId:'474094806386-tkedrgeu1vg8c3ug36edvb4gkhc3a87r.apps.googleusercontent.com',
+        //scope:'https://www.googleapis.com/auth/calendar'
+      })
+    })*/
    }
+
+   Testcalendar(){
+    //FUNZIONA DOPO LOGIN
+    //NON è POSSIBILE INIZIALIZZARE VARIABILE COME FATTO PER AUTH2?
+    //OGNI VOLTA DEVO FARE IL LOAD?
+    //INOLTRE LO SCOPE 
+     gapi.client.load('calendar', 'v3').then(function () {
+       gapi.client.calendar.calendarList.list().then(response => console.log(response)).catch(e => console.log('ERROR LISTING CALENDARS', e))
+     }).catch(e => console.log('ERRORE LOAD CALENDAR API', e))
+     
+   }
+
+   createEventinCalendar(){
+     gapi.client.load('calendar','v3').then(function(){
+       let eventinput:gapi.client.calendar.EventInput={
+         summary:'creato da todo app :)',
+         start:{
+           date:moment(Date.now()).format('YYYY-MM-DD'),
+           //dateTime:moment(Date.now()).format('YYYY-MM-DD h:mm:ss'),
+           timeZone:'Europe/Rome'
+         },
+         end:{
+           date:moment(hoursfromnow(2)).format('YYYY-MM-DD'),
+           //dateTime:moment(hoursfromnow(2)).format('YYYY-MM-DD h:mm:ss'),
+           timeZone:'Europe/Rome'
+
+         }
+       }
+      
+      gapi.client.calendar.events.insert({
+        calendarId:'primary',
+        resource:eventinput,
+        sendNotifications:true,
+      }).then((response:any)=>console.log(response)).catch((error:any)=>console.log('ERRORE INSERT',error))
+      
+      
+     })
+   }
+
+    
+
+   
 
    get Googleuserlogged(){
      return this.googleloggeduser
@@ -26,12 +85,12 @@ export class GoogleService {
      return this.auth2!.signIn({
 
      }).then(user=>{
-       debugger
        console.log(user)
        let id_token=user.getAuthResponse().id_token
       
       this.subject.next(user)//viene emesso valore da observable
       this.googleloggeduser=user
+      window.sessionStorage.setItem('externallogin',String(true))
       window.sessionStorage.setItem('googleuser',JSON.stringify(user))
       //se queste due istruzioni sono invertite causa problemi al corretto render della pagina todo 
       
@@ -48,6 +107,8 @@ export class GoogleService {
      this.auth2?.signOut().then(()=>{
        this.subject.next(null)
        window.sessionStorage.removeItem('googleuser')
+       window.sessionStorage.removeItem('externallogin')
+       window.sessionStorage.removeItem('tok')
        this.ngzone.run(()=>this.router.navigate(['home']))
 
      })
@@ -58,3 +119,5 @@ export class GoogleService {
      return this.subject.asObservable()
    }
 }
+
+const hoursfromnow=(n:any)=>new Date(Date.now()+n*1000*60*60)
