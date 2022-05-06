1214ae51c2012a4988e7cda3b983ec03c44eb145
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2022-05-06 18:21:10 +0200

    aggiunta property userid a interfaccia todo, reso nullable id

--- src/interfaces/todointeerface.ts
+++ src/interfaces/todointeerface.ts
@@ -1,8 +1,9 @@
 export interface Itodointerface{
-    id:number,
+    id?:number,
     title:string,
     created:string,
     isTodoDone:boolean,
     description:string,
-    lastmodified:string
+    lastmodified:string,
+    userid:string
 }
